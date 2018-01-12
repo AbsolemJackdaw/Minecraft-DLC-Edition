1c271e656efc369fb4073884e78f378518f1ac32
MinecraftWero <isaactobalinaf@gmail.com>
2018-01-12 15:56:18 -0600

    Updated mappings and added status message when trying to mine ores

--- src/main/java/subhaven/dlc/events/EventsHandler.java
+++ src/main/java/subhaven/dlc/events/EventsHandler.java
@@ -1,7 +1,12 @@
 package subhaven.dlc.events;
 
+import ibxm.Player;
 import net.minecraft.block.Block;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -15,6 +20,9 @@ public class EventsHandler {
 
         if (block == Blocks.COAL_ORE || block == Blocks.REDSTONE_ORE || block == Blocks.LIT_REDSTONE_ORE || block == Blocks.LAPIS_ORE || block == Blocks.IRON_ORE || block == Blocks.GOLD_ORE ||block == Blocks.DIAMOND_ORE || block == Blocks.EMERALD_ORE || block == Blocks.QUARTZ_ORE){
             event.setCanceled(true);
+            event.getEntityPlayer().sendStatusMessage(new TextComponentString("You need the MineMinerals! pack to play here!"), true); // TODO add custom message
         }
+
     }
 }
+
